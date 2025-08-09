1b7974e0c961685962fc8d6b188a7e2ac979a13c
Nazar <2015VMr.Nazar@gmail.com>
2025-08-09 17:39:30 +0200

    Fixed qr-generator

--- src/coder.jsx
+++ src/coder.jsx
@@ -16,7 +16,7 @@ export default function QRCodeGenerator() {
       return;
     }
 
-    const response = await fetch('/generate_qr', {
+    const response = await fetch('http://localhost:5002/generate_qr', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ text }),
